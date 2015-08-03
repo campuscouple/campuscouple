4b82d06a10f6721179ab32a72c197e61cde61162
nango <nango@qq.com>
2015-08-03 18:24:37 +0800

    update ForgetPassword_Reset

--- src/scut/farseer/campuscouple/activity/ForgetPassword_ResetActivity.java
+++ src/scut/farseer/campuscouple/activity/ForgetPassword_ResetActivity.java
@@ -1,15 +1,177 @@
 package scut.farseer.campuscouple.activity;
 
+import org.json.JSONObject;
+
+import scut.farseer.campuscouple.HttpTask;
 import scut.farseer.campuscouple.R;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class ForgetPassword_ResetActivity extends Activity
+public class ForgetPassword_ResetActivity extends Activity implements
+		android.view.View.OnClickListener
 {
+	private EditText et_password;
+	private EditText et_password_confirm;
+	private Button btn_next;
+	private View sp_back;
+	private boolean flags4Next = false;// 判断能否点击下一步
+	private String verifyCode = "";
+	private String userMobile = "";
+	private String password = "";
+
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.retrieve_password_reset);
+
+		et_password = (EditText) findViewById(R.id.set_password);
+		et_password_confirm = (EditText) findViewById(R.id.confirm_password);
+		btn_next = (Button) findViewById(R.id.next);
+		sp_back = findViewById(R.id.sp_back);
+
+		btn_next.setOnClickListener(this);
+		sp_back.setOnClickListener(this);
+
+		Intent intentFromVerify = getIntent();
+		verifyCode = intentFromVerify.getStringExtra("verify_code");
+
 	}
 
+	@Override
+	public void onClick(View v)
+	{
+		Intent intent = null;
+		switch (v.getId())
+		{
+			case R.id.next:
+				String passwordString = et_password.getText().toString();
+				String confirmPasswordString = et_password_confirm.getText()
+						.toString();
+				checkPassword(passwordString, confirmPasswordString);
+				if (flags4Next)
+				{
+					passwordVerify2Server();
+				}
+				break;
+			case R.id.sp_back:
+				// 返回登陆页面？？
+				// TODO: ？？
+				intent = new Intent(ForgetPassword_ResetActivity.this,
+						LoginActivity.class);
+				startActivity(intent);
+				finish();
+				break;
+			default:
+				break;
+		}
+
+	}
+
+	private void passwordVerify2Server()
+	{
+		Intent intent;
+		// 发送数据到后台,veriry_code,mobile,password
+		HttpTask task = new HttpTask(this) {
+
+			@Override
+			public void callback(String apiUrl, JSONObject jo)
+			{
+				if (jo != null)
+				{
+					try
+					{
+						String warningString = "";
+						int status = jo.getJSONObject("json").getInt("status");
+						Intent intent = null;
+						switch (status)
+						{
+							case 200:
+								intent = new Intent(
+										ForgetPassword_ResetActivity.this,
+										LoginActivity.class);
+								intent.putExtra("user_mobile", userMobile);
+								startActivity(intent);
+								finish();
+								break;
+							case 130:
+								warningString = "密码长度过短";
+								break;
+							case 131:
+								warningString = "密码长度过长";
+								break;
+							case 132:
+								warningString = "密码字符类型不合法";
+								break;
+							case 111:
+								warningString = "参数值类型非法";
+								break;
+							case 110:
+								warningString = "缺少必填参数";
+								break;
+							case 120:
+								warningString = "手机号长度或者类型不正确";
+								break;
+							case 431:
+								warningString = "验证码错误";
+								break;
+							case 421:
+								warningString = "手机号不存在";
+								break;
+							default:
+								warningString = "系统错误";
+								break;
+						}
+
+						if (status != 200)
+							Toast.makeText(ForgetPassword_ResetActivity.this,
+									warningString, Toast.LENGTH_SHORT).show();
+					}
+					// TODO:存储usermobile
+
+					catch (Exception e)
+					{
+						Toast.makeText(ForgetPassword_ResetActivity.this,
+								"系统异常", Toast.LENGTH_SHORT).show();
+					}
+				}
+				else
+					Toast.makeText(ForgetPassword_ResetActivity.this,
+							"json null", Toast.LENGTH_SHORT).show();
+			}
+		};
+
+		task.url("/user/password/verify").addParam("mobile", userMobile)
+				.addParam("verify_code", verifyCode)
+				.addParam("new_password", password).sendRequest();
+	}
+
+	private void checkPassword(String passwordString,
+			String confirmPasswordString)
+	{
+		String messageTaostString = "";
+		if (passwordString == null || passwordString == null)
+		{
+			messageTaostString = "请全部填写";
+			flags4Next = false;
+		}
+		else if (passwordString != passwordString)
+		{
+			messageTaostString = "两次密码不正确";
+			flags4Next = false;
+		}
+		else if (passwordString == passwordString)
+		{
+			password = passwordString;
+			flags4Next = true;
+		}
+
+		if (!flags4Next)
+			Toast.makeText(ForgetPassword_ResetActivity.this,
+					messageTaostString, Toast.LENGTH_SHORT).show();
+	}
 }
--- src/scut/farseer/campuscouple/activity/ForgetPassword_VerifyActivity.java
+++ src/scut/farseer/campuscouple/activity/ForgetPassword_VerifyActivity.java
@@ -1,5 +1,8 @@
 package scut.farseer.campuscouple.activity;
 
+import org.json.JSONObject;
+
+import scut.farseer.campuscouple.HttpTask;
 import scut.farseer.campuscouple.MessageTimeCount;
 import scut.farseer.campuscouple.R;
 import android.app.Activity;
@@ -18,6 +21,9 @@ public class ForgetPassword_VerifyActivity extends Activity implements
 	TextView send_verification_code;
 	Button btn_next;
 	private MessageTimeCount timeCount = null;
+	private String userMobile = "";
+	private boolean flags4Verify = false;// 检查能否发送验证码给后台
+	private String verifyCode = "";
 
 	protected void onCreate(Bundle savedInstanceState)
 	{
@@ -54,48 +60,123 @@ public class ForgetPassword_VerifyActivity extends Activity implements
 			case R.id.send_verification_code:
 
 				// 获取用户输入的手机号码
-				String userMobile = phone_number_input.getText().toString();
+				userMobile = phone_number_input.getText().toString();
 
 				if (userMobile == null || userMobile.length() != 11)// 错误提示
 					userMobileWrongInput(userMobile);
 				else
 				{// 控件不可点击，且倒计时显示“59s后可重发”
 					timeCount.start();
+
 					// 向uer/password/forget发送信息,传入usermobile
 					sendVerifyCode(userMobile);
 
-					// 将输入验证码框改成可输入
-					verification_code_input.setClickable(true);
 					// 将下一步的按钮改成可点击
 					btn_next.setClickable(true);
 				}
 				break;
 
 			case R.id.btn_next:
-				intent = new Intent(this, ForgetPassword_ResetActivity.class);
-				startActivity(intent);
-				finish();
+				// 检查mobile和verify是否填写
+				checkMobileAndVerifyCode();
+				if (flags4Verify)// 向后台发送信息并返回
+				{
+					// 储存Verify_code
+					intent = new Intent(ForgetPassword_VerifyActivity.this,
+							ForgetPassword_ResetActivity.class);
+					intent.putExtra("verify_code", verifyCode);
+					intent.putExtra("user_mobile", userMobile);
+					startActivity(intent);
+					finish();
+				}
 				break;
 			default:
 				break;
 		}
 	}
 
+	// 检查手机号、验证码是否合法，非法则toast显示，否则falgs4Verify为true
+	private void checkMobileAndVerifyCode()
+	{
+		String warningString = "";
+		flags4Verify = false;
+		verifyCode = send_verification_code.getText().toString();
+		// 判断验证码
+		if (verifyCode != null && verifyCode.length() < 20)
+			flags4Verify = true;
+		else
+		{
+			flags4Verify = false;
+			warningString = "验证码格式错误.";
+		}
+
+		// 判断手机号
+		if (userMobile == null || userMobile.length() != 11)
+		{
+			warningString = "手机号不正确.";
+			flags4Verify = false;
+		}
+		else if (userMobile != phone_number_input.getText().toString())
+			phone_number_input.setText(userMobile);
+
+		// 非法则toast提示
+		if (!flags4Verify)
+			Toast.makeText(ForgetPassword_VerifyActivity.this, warningString,
+					Toast.LENGTH_LONG).show();
+	}
+
 	private void userMobileWrongInput(String userMobile)
 	{
 		String warningString = "";
 		if (userMobile == null)
-			warningString = "未输入用户手机号码.";
+			warningString = "未输入手机号码.";
 		else
 			warningString = "不存在该手机号，长度不对.";
-		Toast.makeText(ForgetPassword_VerifyActivity.this, warningString,
-				Toast.LENGTH_LONG).show();
+		if (userMobile.length() != 11)
+			Toast.makeText(ForgetPassword_VerifyActivity.this, warningString,
+					Toast.LENGTH_LONG).show();
 	}
 
 	// 向uer/password/forget发送信息,传入usermobile
 	private void sendVerifyCode(String userMobile)
 	{
+		HttpTask task = new HttpTask(this) {
 
+			@Override
+			public void callback(String apiUrl, JSONObject jo)
+			{
+				if (jo != null)
+				{
+					try
+					{
+						String warningString = "";
+						int status = jo.getJSONObject("json").getInt("status");
+						if (status != 200)
+						{
+							if (status == 421)
+								warningString = "手机号不存在";
+							else if (status == 700 || status != 200)
+								warningString = "系统错误";
+							Toast.makeText(ForgetPassword_VerifyActivity.this,
+									warningString, Toast.LENGTH_SHORT).show();
+						}
+						// TODO:存储usermobile
+
+					}
+					catch (Exception e)
+					{
+						Toast.makeText(ForgetPassword_VerifyActivity.this,
+								"系统异常", Toast.LENGTH_SHORT).show();
+					}
+				}
+				else
+					Toast.makeText(ForgetPassword_VerifyActivity.this,
+							"json null", Toast.LENGTH_SHORT).show();
+			}
+		};
+
+		task.url("/user/password/forget").addParam("mobile", userMobile)
+				.sendRequest();
 	}
 
 	public void onBackPressed()
